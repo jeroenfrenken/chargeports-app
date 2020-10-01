59d5f64f86bf724d3f1053aafb8bb433f6465774
Jeroen Frenken <jeroenfrenken@icloud.com>
2020-10-01 12:51:01 +0200

    Removed liines

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,10 +1,8 @@
 import { createTypedHooks } from 'easy-peasy';
 import { StoreModel } from './model';
 
-// Provide our model to the helper      👇
 const typedHooks = createTypedHooks<StoreModel>();
 
-// 👇 export the typed hooks
 export const useStoreActions = typedHooks.useStoreActions;
 export const useStoreDispatch = typedHooks.useStoreDispatch;
 export const useStoreState = typedHooks.useStoreState;
