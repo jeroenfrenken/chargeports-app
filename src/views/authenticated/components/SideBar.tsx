928db68c621716583dc65ce94d50fd5d2df0a235
Johan de Lijser <johan@lyfter.nl>
2020-10-25 12:26:21 +0100

    Set right app name and show it in menu, also show version number

--- src/views/authenticated/components/SideBar.tsx
+++ src/views/authenticated/components/SideBar.tsx
@@ -3,20 +3,28 @@ import React from 'react';
 import { Button, Text, View } from 'react-native';
 import { User } from '../../../api';
 import { useStoreState } from '../../../store/hooks';
+import Constants from 'expo-constants';
 
 export function SideBar(props) {
     const user: User = useStoreState(state => state.auth.user);
 
     return (
         <DrawerContentScrollView {...props}>
-            <Text style={{
-                textAlign: "center",
-                fontSize: 18,
-                padding: 40
-            }}>{user.firstName} {user.lastName}</Text>
+            <View>
+                <Text style={{
+                    textAlign: "center",
+                    fontSize: 18,
+                    padding: 40
+                }}>{user.firstName} {user.lastName}</Text>
+            </View>
             <DrawerItemList {...props} />
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-end' }}>
-                <Button onPress={() => props.logout()} title="Logout"/>
+                <Button onPress={() => props.logout()} title="Uitloggen"/>
+                <Text style={{
+                    textAlign: "center",
+                    fontSize: 14,
+                    fontWeight: "300"
+                }}>{ Constants.manifest.name } { Constants.manifest.version }</Text>
             </View>
         </DrawerContentScrollView>
     );
