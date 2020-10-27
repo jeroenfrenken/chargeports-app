e720132b3e4a3dda54ccb624c0149cacbd4413b9
JohanDeLijser <johan@lyfter.nl>
2020-10-27 20:17:56 +0100

    Update menu interface #41 (#6)
    * Set right app name and show it in menu, also show version number
    
    * Add Previous charges page
    
    * Add settings page
    
    * Add upcomming charges page
    
    * Add pages and changes to sidebar menu
    
    * Add button styling
    
    * Remove unnes styling

--- src/views/authenticated/screens/AuthenticatedScreen.tsx
+++ src/views/authenticated/screens/AuthenticatedScreen.tsx
@@ -4,7 +4,16 @@ import { useStoreActions } from '../../../store/hooks';
 import { SideBar } from '../components/SideBar';
 // @ts-ignore
 import MapIcon from "../../../assets/icons/MapIcon.svg";
+// @ts-ignore
+import Previous from "../../../assets/icons/Previous.svg";
+// @ts-ignore
+import Upcomming from "../../../assets/icons/Upcomming.svg";
+// @ts-ignore
+import Settings from "../../../assets/icons/Settings.svg";
 import MapScreenNavigator from './MapScreenNavigator';
+import PreviousChargesScreen from "./PreviousChargesScreen";
+import UpcommingChargesScreen from "./UpcommingChargesScreen";
+import SettingsScreen from "./SettingsScreen";
 
 const Drawer = createDrawerNavigator();
 
@@ -14,7 +23,7 @@ export default function AuthenticatedScreen() {
     return (
         <Drawer.Navigator initialRouteName="Map" drawerContent={(props) => (
             <SideBar {...props} logout={logoutAction} />
-        )}>
+        )} drawerStyle={{backgroundColor: "transparent"}}>
             <Drawer.Screen
                 name="Map"
                 component={MapScreenNavigator}
@@ -22,6 +31,36 @@ export default function AuthenticatedScreen() {
                     title: 'Map',
                     drawerIcon: ({focused, size}) => (
                         <MapIcon />
+                    ),
+                }}
+            />
+            <Drawer.Screen
+                name="PreviousCharges"
+                component={PreviousChargesScreen}
+                options={{
+                    title: 'Vorige laadbeurten',
+                    drawerIcon: ({focused, size}) => (
+                        <Previous />
+                    ),
+                }}
+            />
+            <Drawer.Screen
+                name="UpcommingCharges"
+                component={UpcommingChargesScreen}
+                options={{
+                    title: 'Aankomende laadbeurten',
+                    drawerIcon: ({focused, size}) => (
+                        <Upcomming />
+                    ),
+                }}
+            />
+            <Drawer.Screen
+                name="Settings"
+                component={SettingsScreen}
+                options={{
+                    title: 'Instellingen',
+                    drawerIcon: ({focused, size}) => (
+                        <Settings />
                     ),
                 }}
             />
